4dc6fe14b77b6658d5594eb433d7f45eb90c02f5
Johan <Johan@10.22.1.207>
2018-01-24 14:31:51 +0100

    startar

--- src/orukomm/OruKomm.java
+++ src/orukomm/OruKomm.java
@@ -15,6 +15,7 @@ public class OruKomm {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String hej;
         // TODO code application logic here
     }
     
