e822c6709967e351b05e89e313a59fd3d0211c55
Johan <Johan@10.22.12.183>
2018-01-30 15:40:33 +0100

    Fixat nya tables för attachments och posts Fixat FileStorage klass för convertering av filer till blob

--- src/orukomm/gui/MainWindow.java
+++ src/orukomm/gui/MainWindow.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.util.EnumSet;
 import javax.swing.JPanel;
 import orukomm.Settings;
+import orukomm.data.FileStorage;
 import orukomm.data.entities.User;
 import orukomm.data.entities.User.PermissionFlag;
 import orukomm.gui.panels.Login;
@@ -12,93 +13,100 @@ import orukomm.gui.panels.Register;
 
 public class MainWindow extends javax.swing.JFrame implements ActionListener {
 
-	// Global user object of the logged in user.
-	public User loggedInUser = new User();
-	
-	public MainWindow() {
-		setTitle(Settings.WINDOW_TITLE);
-		setResizable(false);
-		initComponents();
-		initPanels();
-		setLocationRelativeTo(null);
-		setVisible(true);
-		switchPanel(new Login(this));
-		addActionListeners();
-		enableLoggedInInterface(Settings.LOGGED_OUT_ROLE);
-		
-	}
-	
-	private void initPanels() {
-		pnlContainer.setVisible(true);
-	}
+    // Global user object of the logged in user.
+    public User loggedInUser = new User();
 
-	/*
+    public MainWindow() {
+        setTitle(Settings.WINDOW_TITLE);
+        setResizable(false);
+        initComponents();
+        initPanels();
+        setLocationRelativeTo(null);
+        setVisible(true);
+        switchPanel(new Login(this));
+        addActionListeners();
+        enableLoggedInInterface(Settings.LOGGED_OUT_ROLE);
+        
+        
+        FileStorage fs = new FileStorage();
+        fs.readFile("C:\\server\\images\\bild.jpg");
+        fs.insertFile(1, "C:\\server\\images\\bild.jpg");
+        fs.convertToBlob(1, "C:\\server\\images\\bildblob.jpg");
+        
+        
+    }
+
+    private void initPanels() {
+        pnlContainer.setVisible(true);
+    }
+
+    /*
 	 * Enables and disables GUI components for a user role, depending on its permissions.
-	 */
-	public void enableLoggedInInterface(EnumSet<PermissionFlag> userRole) {
-		boolean hasUserPermission = userRole.contains(PermissionFlag.USER);
-		boolean hasAdminPermission = userRole.contains(PermissionFlag.ADMIN);
-		boolean hasSuperadminPermission = userRole.contains(PermissionFlag.SUPERADMIN);
-		
-		mnuAccount.setVisible(hasUserPermission);
-		mnuAdministration.setVisible(hasSuperadminPermission);
-	}
+     */
+    public void enableLoggedInInterface(EnumSet<PermissionFlag> userRole) {
+        boolean hasUserPermission = userRole.contains(PermissionFlag.USER);
+        boolean hasAdminPermission = userRole.contains(PermissionFlag.ADMIN);
+        boolean hasSuperadminPermission = userRole.contains(PermissionFlag.SUPERADMIN);
 
-	/*
+        mnuAccount.setVisible(hasUserPermission);
+        mnuAdministration.setVisible(hasSuperadminPermission);
+    }
+
+    /*
 	 * Removes current panel attached to the panel container and adds the specified panel to it.
-	 */
-	public void switchPanel(JPanel panel) {
-		pnlContainer.removeAll();
-		pnlContainer.repaint();
-		pnlContainer.revalidate();
-		pnlContainer.add(panel);
-		pnlContainer.repaint();
-		pnlContainer.revalidate();
-	}
-	
-	private void addActionListeners() {
-		mnuArchiveExit.setActionCommand("mnuArchiveExit");
-		mnuArchiveExit.addActionListener(this);
-		
-		mnuAdministrationRegister.setActionCommand("mnuAdministrationRegister");
-		mnuAdministrationRegister.addActionListener(this);
-		
-		mnuAccountLogout.setActionCommand("mnuAccountLogout");
-		mnuAccountLogout.addActionListener(this);
-	}
+     */
+    public void switchPanel(JPanel panel) {
+        pnlContainer.removeAll();
+        pnlContainer.repaint();
+        pnlContainer.revalidate();
+        pnlContainer.add(panel);
+        pnlContainer.repaint();
+        pnlContainer.revalidate();
+    }
 
-	/*
+    private void addActionListeners() {
+        mnuArchiveExit.setActionCommand("mnuArchiveExit");
+        mnuArchiveExit.addActionListener(this);
+
+        mnuAdministrationRegister.setActionCommand("mnuAdministrationRegister");
+        mnuAdministrationRegister.addActionListener(this);
+
+        mnuAccountLogout.setActionCommand("mnuAccountLogout");
+        mnuAccountLogout.addActionListener(this);
+    }
+
+    /*
 	 * Handle menu events.
-	 */
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		switch (e.getActionCommand()) {
-			case "mnuArchiveExit":
-				this.dispatchEvent(new java.awt.event.WindowEvent(this, java.awt.event.WindowEvent.WINDOW_CLOSING));
-				break;
-			
-			case "mnuAdministrationRegister":
-				switchPanel(new Register((this)));
-				break;
-				
-			case "mnuAccountLogout":
-				logout();
-				break;
-		}
-	}
-	
-	/*
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "mnuArchiveExit":
+                this.dispatchEvent(new java.awt.event.WindowEvent(this, java.awt.event.WindowEvent.WINDOW_CLOSING));
+                break;
+
+            case "mnuAdministrationRegister":
+                switchPanel(new Register((this)));
+                break;
+
+            case "mnuAccountLogout":
+                logout();
+                break;
+        }
+    }
+
+    /*
 	 * Reset logged in user object, wipe out the old one, and set the appropriate GUI.
-	 */
-	private void logout() {
-		loggedInUser = new User();
-		System.gc();
-		enableLoggedInInterface(Settings.LOGGED_OUT_ROLE);
-		setTitle(Settings.WINDOW_TITLE);
-		switchPanel(new Login((this)));
-	}
-	
-	@SuppressWarnings("unchecked")
+     */
+    private void logout() {
+        loggedInUser = new User();
+        System.gc();
+        enableLoggedInInterface(Settings.LOGGED_OUT_ROLE);
+        setTitle(Settings.WINDOW_TITLE);
+        switchPanel(new Login((this)));
+    }
+
+    @SuppressWarnings("unchecked")
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
 
