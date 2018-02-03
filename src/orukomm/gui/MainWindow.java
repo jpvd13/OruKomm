46f44acb5f7d22231b42eee2990b7cb1e054c909
solecki <ad.solecki@gmail.com>
2018-02-03 09:59:13 +0100

    Cleanup

--- src/orukomm/gui/MainWindow.java
+++ src/orukomm/gui/MainWindow.java
@@ -117,7 +117,7 @@ public class MainWindow extends javax.swing.JFrame implements ActionListener {
                 break;
 
             case "mnuAdministrationAddCategory":
-                new AddCategory(this, false).setVisible(true);
+                new AddCategory(this, true).setVisible(true);
                 break;
 
             case "mnuAdministrationEditUser":
--- src/orukomm/gui/dialogs/AddCategory.java
+++ src/orukomm/gui/dialogs/AddCategory.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 import orukomm.data.entities.Category;
 import orukomm.data.repositories.CategoryRepository;
+import orukomm.gui.MainWindow;
 import orukomm.logic.security.Validation;
 
 public class AddCategory extends javax.swing.JDialog {
@@ -12,7 +13,7 @@ public class AddCategory extends javax.swing.JDialog {
     private CategoryRepository categoryRepo;
     private Category category;
 
-    public AddCategory(java.awt.Frame parentFrame, boolean modal) {
+    public AddCategory(MainWindow parentFrame, boolean modal) {
         super(parentFrame, modal);
         categoryRepo = new CategoryRepository();
         category = new Category();
@@ -105,48 +106,6 @@ public class AddCategory extends javax.swing.JDialog {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AddCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AddCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AddCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AddCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                AddCategory dialog = new AddCategory(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAddCategory;
